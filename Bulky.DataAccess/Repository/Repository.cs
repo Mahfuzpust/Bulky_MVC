e12b3e393df0f06e3aed4e25cd52efa2bafb055c
mahfuz khan <mahfuzpust@gmail.com>
2023-07-30 22:08:59 +0600

    Repository-IRepository-Class-Interface

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bulky.DataAccess.Repository.IRepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace Bulky.DataAccess.Repository
 {
-    internal class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(System.Linq.Expressions.Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
