8dd238a4c41731374ec5d301bce6612a350753a2
mahfuz khan <mahfuzpust@gmail.com>
2023-08-02 09:56:13 +0600

    Update

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -37,5 +38,7 @@ namespace Bulky.Models
         [Display(Name = "Price for 100+")]
         [Range(1, 100000)]
         public double Price100 { get; set; }
+
+       
     }
 }
