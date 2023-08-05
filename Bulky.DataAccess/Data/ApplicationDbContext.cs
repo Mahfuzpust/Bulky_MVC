8e5a49925c7952c0ebdbb6f6d81e1c36f5297d17
mahfuz khan <mahfuzpust@gmail.com>
2023-08-06 00:46:36 +0600

    Identity Update

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -18,64 +18,7 @@ namespace Bulky.DataAccess.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Product>().HasData(
-                new Product
-                {
-                    Id = 1,
-                    Title = "Fortune of Time",
-                    Author = "Billy Spark",
-                    Description = "A model may be static or dynamic. In a static model, a single variable is taken as a key element for calculating cost and time.",
-                    ISBN = "SWB012345698",
-                    ListPrice =99,
-                    Price = 90,
-                    Price50 = 80,
-                    Price100 = 80,
-                    CategoryId = 1,
-                    ImageUrl = ""
-                },
-                new Product
-                {
-                    Id = 2,
-                    Title = "Fortune ",
-                    Author = "Billy ",
-                    Description = "A model may be static or dynamic. In a static model, a single variable is taken as a key element for calculating cost and time.",
-                    ISBN = "SWB012345698",
-                    ListPrice = 90,
-                    Price = 85,
-                    Price50 = 80,
-                    Price100 = 80,
-                    CategoryId = 2,
-                    ImageUrl = ""
-                },
-                new Product
-                {
-                    Id = 3,
-                    Title = " of Time",
-                    Author = "Billy Spark",
-                    Description = "A model may be static or dynamic. In a static model, a single variable is taken as a key element for calculating cost and time.",
-                    ISBN = "SWB012345698",
-                    ListPrice = 89,
-                    Price = 50,
-                    Price50 = 40,
-                    Price100 = 80,
-                    CategoryId = 3,
-                    ImageUrl=""
-                },
-                new Product
-                {
-                    Id = 4,
-                    Title = "Fortune in Time",
-                    Author = "Billy Spark",
-                    Description = "A model may be static or dynamic. In a static model, a single variable is taken as a key element for calculating cost and time.",
-                    ISBN = "SWB012345698",
-                    ListPrice = 99,
-                    Price = 90,
-                    Price50 = 80,
-                    Price100 = 80,
-                    CategoryId = 4,
-                    ImageUrl = ""
-                }
-                );
+            
         }
     }
 }
