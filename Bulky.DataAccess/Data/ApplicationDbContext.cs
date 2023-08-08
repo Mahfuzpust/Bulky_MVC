0548cd07c4711e19d7c9352da4850b3cc8a7a512
mahfuz khan <mahfuzpust@gmail.com>
2023-08-08 20:29:35 +0600

    Company Details

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -13,12 +13,41 @@ namespace Bulky.DataAccess.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Company> Companies { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            
+            modelBuilder.Entity<Company>().HasData(
+                new Company
+                {
+                    Id = 6,
+                    Name = "Test",
+                    StreetAddress = "mahfuz 123",
+                    City = "Test",
+                    State = "Test",
+                    PhoneNumber = "Test",
+                },
+                 new Company
+                 {
+                     Id = 7,
+                     Name = "Test",
+                     StreetAddress = "mahfuz 123",
+                     City = "Test",
+                     State = "Test",
+                     PhoneNumber = "Test",
+                 },
+                 new Company
+                 {
+                     Id = 8,
+                     Name = "Test",
+                     StreetAddress = "mahfuz 123",
+                     City = "Test",
+                     State = "Test",
+                     PhoneNumber = "Test",
+                 }  
+                );
         }
     }
 }
---
+++ Bulky.DataAccess/Migrations/20230808110618_CampanyTableRecord.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class CampanyTableRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "City", "Name", "PhoneNumber", "PostalCode", "State", "StreetAddress" },
+                values: new object[,]
+                {
+                    { 6, "Test", "Test", "Test", null, "Test", "mahfuz 123" },
+                    { 7, "Test", "Test", "Test", null, "Test", "mahfuz 123" },
+                    { 8, "Test", "Test", "Test", null, "Test", "mahfuz 123" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 8);
+        }
+    }
+}
